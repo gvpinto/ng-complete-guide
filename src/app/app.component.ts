9b1ab95cd301579c1797d4d4d918eb1992c85f35
Glenn Pinto <gvpinto@gmail.com>
2021-11-08 20:31:43 -0500

    86. Adding Navigation with Event Binding and ngIf - My Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  menu: string = 'recipes';
+
+  showMenu(selectedMenu: string) {
+    this.menu = selectedMenu;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() selectedMenu = new EventEmitter<string>();
+
+  onRecipesMenu(): void {
+    this.selectedMenu.emit('recipes');
+  }
+
+  onShoppingListMenu(): void {
+    this.selectedMenu.emit('shoppingList');
+  }
+}
